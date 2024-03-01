2607aed22981c7468dd4b50a95c6154c20f96b9c
Prady <gabrielpac@al.insper.edu.br>
2024-03-01 20:31:07 +0000

    adder2bits

--- ula_modules.py
+++ ula_modules.py
@@ -27,18 +27,25 @@ def fullAdder(a, b, c, soma, carry):
     # s2 = Signal(bool(0))
     # s3 = Signal(bool(0))
 
-    half_1 = halfAdder(a, b, s[1], s[2])
-    half_2 = halfAdder(c, s[1], soma, s[3])
+    half_1 = halfAdder(a, b, s[0], s[1])
+    half_2 = halfAdder(c, s[0], soma, s[2])
 
     @always_comb
     def comb():
-        carry.next = s[2] or s[3]
+        carry.next = s[1] or s[2]
 
     return instances()
 
 
 @block
 def adder2bits(x, y, soma, carry):
+    c = Signal(bool(0))
+    half = halfAdder(x[0], y[0], soma[0], c)
+    
+    full = fullAdder(x[1], y[1], c, soma[1], carry)
+
+
+
     return instances()
 
 
